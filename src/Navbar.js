f1fb1fa79a6c744735b38109d5e87bc7d8ef0595
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-10-30 17:23:50 +0500

    classNames fixed

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,17 +4,17 @@ import { BellFilled, CaretDownOutlined, DownOutlined, PlusOutlined, SearchOutlin
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="col-1">
-        <button className="btn">
+      <div className="col col-1">
+        <button className="btn project-btn">
           My Projects
           <DownOutlined className="chevron-down-icon" />
         </button>
-        <button className="btn">
+        <button className="btn create-btn">
           Create
           <PlusOutlined className="plus-icon" />
         </button>
       </div>
-      <div className="col-2">
+      <div className="col col-2">
         <div className="input">
           <SearchOutlined className="search-icon" />
           <input className="search" type="text" placeholder="Search" />
